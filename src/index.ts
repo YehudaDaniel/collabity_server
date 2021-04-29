8edb298b8b51afbfd2eb3de64ed6b9e5e8edf0fb
h10ytguf <h10ytguf@gmail.com>
2021-04-29 15:28:37 +0300

    data from app.ts moved to index.ts, added routes configuration function and database connection function, fixed auth.middleware to find one by id and token, auth now fully working.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import app from './app'
+import express from 'express';
+import { userRouter } from './routers/users.routers'
+import { DatabaseDriver } from './db/mongoose.db'
+import { RoutesConfig } from './config/routes.config';
 
+const app = express()
 const PORT = process.env.PORT
+
+DatabaseDriver.connect()
+
+//Tells express to parse the json data coming, into an object and be accessable via request.body
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+//Includes the user router and uses its router request
+app.use(userRouter)
+RoutesConfig(app)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,22 +1,27 @@
 import { NextFunction } from 'express'
 import * as jwt from 'jsonwebtoken'
 import { User } from '../models/users.models'
+import { ValidJWT } from '../utils/interfaces/ValidJWT.interfaces'
+import { ResponseStatus } from '../utils/status.utils'
 
-const auth = async (req:any, res:any, next:NextFunction):Promise<void> => {
+export const auth = async (req:any, res:any, next:NextFunction):Promise<void> => {
     try{
         const token: string = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string)
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        const verifiedJWT = jwt.verify(token, process.env.JWT_SECRET as string)
+        
+        if (typeof verifiedJWT === 'object') {
+            let decoded: ValidJWT = verifiedJWT as ValidJWT;        
+            const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-        if(!user)
-            throw new Error()
-
-        req.token = token
-        req.user = user
-        next()
+            if(!user)
+                throw new Error()
+            req.token = token
+            req.user = user
+            next()
+        } else {
+            throw new Error('no authentication');
+        }
     }catch(e) {
-        res.status(401).send({error: 'Please authenticate'})
+        res.status(ResponseStatus.Unauthorized).send({error: 'Please authenticate'})
     }
 }
-
-export = auth
