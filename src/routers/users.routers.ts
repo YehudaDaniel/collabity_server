369c7ad074ca5bf571883f7f30df16c2fa6517eb
h10ytguf <h10ytguf@gmail.com>
2021-04-29 15:30:25 +0300

    refactored the users router to be more simple

--- src/routers/users.routers.ts
+++ src/routers/users.routers.ts
@@ -1,7 +1,8 @@
 import { Request, Response, Router} from 'express'
-import auth from '../middleware/auth.middleware'
+import { auth } from '../middleware/auth.middleware'
 import { User } from '../models/users.models'
 import { NotationAllowed } from '../utils/interfaces/NotationAllowed.interfaces'
+import { ResponseStatus } from '../utils/status.utils'
 
 //Modules
 export const userRouter: Router = Router()
@@ -9,18 +10,18 @@ export const userRouter: Router = Router()
 //User requests
 userRouter
     //GET Requests
-    .get('/user/me', auth, async (req:any, res: Response) => {
+    .get('/me', auth, async (req:any, res: Response) => {
         res.send(req.user)
     })
     //Post Requests
-    .post('/user/register', async (req: Request, res: Response) => {
+    .post('/register', async (req: Request, res: Response) => {
         const user: NotationAllowed = new User(req.body)
 
         try{
             await user.save()
             const token = await user.generateAuthToken()
-            res.status(201).send({ user, token})
+            res.status(ResponseStatus.Created).send({ user, token})
         }catch(e) {
-            res.status(400).send(e)
+            res.status(ResponseStatus.BadRequest).send(e)
         }
     })
