6ade55ee63e519284ea4e6c9929b5abe0f4f0499
h10ytguf <h10ytguf@gmail.com>
2021-04-29 15:26:08 +0300

    deleted app.ts, restructured mongoode.db to catch errors and end connection

--- src/db/mongoose.db.ts
+++ src/db/mongoose.db.ts
@@ -1,13 +1,43 @@
-import mongoose from 'mongoose'
+import mongoose, { Mongoose } from 'mongoose'
+const url = process.env.DATABASE_URL as string
 
-const url = process.env.DATABASE_URL
 
-mongoose
-    .connect(url as string, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: true
+export module DatabaseDriver {
+    export async function connect() {
+        try{
+            const connected: Mongoose = await connectDB(url)
+            console.log('Successfully connected to MongoDB')
+
+            return connected.connection.readyState == MongooseReadyState.Connected
+        }catch(e) {
+            console.log(`Failed connection to MongoDB: ${e.message}`)
+        }
+    }
+
+
+    async function connectDB(connectionString: string): Promise<Mongoose> {
+        console.log(`Connecting to Database ${connectionString}`)
+        return mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true
+        })
+    }
+}
+
+//closing DB connection if node process was terminated
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose disconnected on app termination')
+        process.exit(0)
     })
-    .then(() => console.log('Database Connected.'))
-    .catch((e) => console.log(e))
+})
+
+
+enum MongooseReadyState {
+    Disconnected,
+    Connected,
+    Connecting,
+    Disconnecting
+}
