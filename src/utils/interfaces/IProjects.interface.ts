796fd2c4acd721813e7415100b70e8dae72d75b3
h10ytguf <h10ytguf@gmail.com>
2021-06-22 14:22:07 +0300

    minor changes to the project and user models interfaces

--- src/utils/interfaces/IProjects.interface.ts
+++ src/utils/interfaces/IProjects.interface.ts
@@ -4,6 +4,6 @@ export interface IProjects extends Document {
     title: string,
     content: string,
     features?: Array<string>,
-    participants?: Array<string>,
+    participants?: Array<Object>,
     owner: Schema.Types.ObjectId
 }
